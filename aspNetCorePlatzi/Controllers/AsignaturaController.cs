28a852c3e5d545c49df81e69b5fb98d03e8cbdbb
fernastereo <fernandoecueto@gmail.com>
2019-07-29 11:07:55 -0500

    Creando enrutamiento en el controlador

--- aspNetCorePlatzi/Controllers/AsignaturaController.cs
+++ aspNetCorePlatzi/Controllers/AsignaturaController.cs
@@ -9,11 +9,29 @@ namespace aspNetCorePlatzi.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index() {
-            ViewBag.Cosadinamica = "Cualquier vaina que quiere enviar a la vista";
-            ViewBag.Fecha = DateTime.Now;
+        [Route("asignatura/index")]
+        [Route("asignatura/index/{asignaturaid}")]
 
-            return View(_context.Asignaturas.FirstOrDefault());
+        //public IActionResult Index() {
+        //    ViewBag.Cosadinamica = "Cualquier vaina que quiere enviar a la vista";
+        //    ViewBag.Fecha = DateTime.Now;
+
+        //    return View(_context.Asignaturas.FirstOrDefault());
+        //}
+
+        public IActionResult Index(string asignaturaid) {
+
+            if (!string.IsNullOrWhiteSpace(asignaturaid))
+            {
+                var asignatura = from asig in _context.Asignaturas
+                                 where asig.Id == asignaturaid
+                                 select asig;
+
+                return View(asignatura.SingleOrDefault());
+            } else
+            {
+                return View("multiasignatura", _context.Asignaturas);
+            }
         }
 
         public IActionResult multiAsignatura()
